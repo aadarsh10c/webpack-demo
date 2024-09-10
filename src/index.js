2093cf1da86833f17ecdabb129308ee815dbc1b0
adarsh010c <adarsh010c@gmail.com>
2024-09-10 17:02:32 +0530

    asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import './style.css'
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a this script,
     element.innerHTML = "Hello Webpack";
-  
+    element.classList.add('hello');
+
     return element;
   }
   
